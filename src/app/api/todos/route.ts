574062d8182c6791f0a179600f6497934f694c45
jesusrobot0 <jesusrobot9000@icloud.com>
2024-02-15 19:18:13 -0600

    Add to de api the endpoint to create a new todo

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/prisma";
+import * as yup from "yup";
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -24,4 +30,20 @@ export async function GET(request: Request) {
   const todos = await prisma.todo.findMany({ skip, take });
 
   return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+  try {
+    const { description, complete } = await postSchema.validate(
+      await request.json()
+    );
+
+    const newTodo = await prisma.todo.create({
+      data: { description, complete },
+    });
+
+    return NextResponse.json(newTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
