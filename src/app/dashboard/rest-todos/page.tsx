7ec9848a8209b2fc39c5f3271dbd0d033601d8cf
jesusrobot0 <jesusrobot9000@icloud.com>
2024-02-21 00:40:56 -0600

    Update to revalidate page data in each request

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import prisma from "@/lib/prisma";
 import { TitlePage } from "@/components";
 import { NewTodo, TodosGrid } from "@/todos/components";
